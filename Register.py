dd96e35dac3d5a8d7b01617a0b1ed58bdcc00c11
H252003 <mohamedhesham252003@gmail.com>
2024-07-23 23:39:48 +0300

    trying vercel

--- Register.py
+++ Register.py
@@ -1,9 +1,7 @@
-
-import os
-
+from database import Users, db
+from extensions import bcrypt
 from flask import request, jsonify, Blueprint
 from flask_cors import CORS
-from werkzeug.utils import secure_filename
 
 
 register = Blueprint("register", __name__, static_folder="static")
@@ -15,6 +13,37 @@ register = Blueprint("register", __name__, static_folder="static")
 def register_1():
 
 """
+
+#register route using sqlalchemy and add the user into the database
+@register.route('/register', methods=['POST'])
+def register_1():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    name = data.get('name')
+    age = data.get('age')
+    email = data.get('email')
+
+    #check if email and password are provided
+    if not email or not password:
+        return jsonify({"error": "Please provide both email and password"}), 400
+
+    #check if user exists in the db
+    user = Users.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"error": "User already exists"}), 400
+
+    #hash the password
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    #add the user to the database
+    new_user = Users(username=username, password=hashed_password, name=name, age=age, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User created successfully"}), 201
+
+
 
 # @register.route('/check_user', methods=['POST'])
 # def check_user():
